70cd21b5b2a81693659070fdcbb4b3daefc4b711
Nasibullah Nasiri <nasibullah.nasiri@telefonica.com>
2025-08-18 13:17:55 +0200

    needed commit

--- apps/frontend/src/components/GameRoom.vue
+++ apps/frontend/src/components/GameRoom.vue
@@ -14,6 +14,7 @@ export default {
     };
   },
   methods: {
+    //
     getUsernameForRoom() {
       const savedRooms = JSON.parse(localStorage.getItem("rooms"));
       if (savedRooms[this.hash] && savedRooms.createdRoom === false) {
