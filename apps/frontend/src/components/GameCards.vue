8fb0aa345959f0204ff94b197d73a81bc92c98c1
Nasibullah Nasiri <nasibullah.nasiri@telefonica.com>
2025-09-03 14:19:01 +0200

    reset selected card when round or reveal ends

--- apps/frontend/src/components/GameCards.vue
+++ apps/frontend/src/components/GameCards.vue
@@ -9,46 +9,55 @@ export default {
   },
   methods: {
     toggleCard(card) {
-      console.log(this.$socketConnect.roundStarted)
-     if(this.$socketConnect.roundStarted){
-      if (this.selectedCard === card) {
-        this.selectedCard = null;
-        this.$emit("card", null);
-      } else {
-        this.selectedCard = card;
-        this.$emit("card", card);
-      }}
-    }
+      if (this.$socketConnect.roundStarted) {
+        if (this.selectedCard === card) {
+          this.selectedCard = null;
+          this.$emit("card", null);
+        } else {
+          this.selectedCard = card;
+          this.$emit("card", card);
+        }
+      }
+    },
   },
   watch: {
-    "$socketConnect.roundStarted"(newVal) {
-      if (newVal) {
+    "$socketConnect.roundStarted"(newVal, oldVal) {
+      if (oldVal === true && newVal === false) {
         this.selectedCard = null;
+        this.$emit("card", null);
       }
-    }
-  }
+    },
+    "$socketConnect.revealCards"(newVal, oldVal) {
+      if (oldVal === true && newVal === false) {
+        this.selectedCard = null;
+        this.$emit("card", null);
+      }
+    },
+  },
 };
 </script>
+
 <template>
   <div class="absolute w-full bottom-30">
-      <div class="flex justify-center items-center">
-  <div
-    v-for="card in fibonacciCards"
-    :key="card"
-    @click="toggleCard(card)"
-    class="w-16 h-24 bg-white rounded-md border-2 border-gray-300 flex items-center justify-center cursor-pointer text-2xl font-bold text-gray-800 relative transition-all duration-200 hover:transform hover:-translate-y-1 hover:shadow-md active:scale-95 m-3"
-    :class="{
-      'border-yellow-400 shadow-lg bg-yellow-50 z-10': selectedCard === card,
-    }"
-  >
-    {{ card }}
-    <div
-      v-if="selectedCard === card"
-      class="absolute top-1 right-1 text-yellow-500 text-sm"
-    >
-      ✓
+    <div class="flex justify-center items-center">
+      <div
+        v-for="card in fibonacciCards"
+        :key="card"
+        @click="toggleCard(card)"
+        class="w-16 h-24 bg-white rounded-md border-2 border-gray-300 flex items-center justify-center cursor-pointer text-2xl font-bold text-gray-800 relative transition-all duration-200 hover:transform hover:-translate-y-1 hover:shadow-md active:scale-95 m-3"
+        :class="{
+          'border-yellow-400 shadow-lg bg-yellow-50 z-10':
+            selectedCard === card,
+        }"
+      >
+        {{ card }}
+        <div
+          v-if="selectedCard === card"
+          class="absolute top-1 right-1 text-yellow-500 text-sm"
+        >
+          ✓
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
   </div>
 </template>
